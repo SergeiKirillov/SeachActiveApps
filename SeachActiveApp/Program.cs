9ac6ceb4580763c0c763c4c7a689690550852eb8
user <user@LPC2AUTOITPROG>
2020-10-17 08:51:36 +0600

    + class write log in file

--- SeachActiveApp/Program.cs
+++ SeachActiveApp/Program.cs
@@ -14,8 +14,28 @@ namespace SeachActiveApp
             clWinAPI.HideConsoleApp(true);
             while (true)
             {
-                DateTime dtNow = DateTime.Now;
-                Console.WriteLine(dtNow.ToString("dd.MM.yyyy HH:mm:ss") + "-" + clWinAPI.GetCaptionOfActiveWindow());
+
+
+                //DateTime dtNow = DateTime.Now;
+                //Console.WriteLine(dtNow.ToString("dd.MM.yyyy HH:mm:ss") + "-" + clWinAPI.GetCaptionOfActiveWindow());
+
+
+                //if (message0 != message)
+                //{
+
+                //    tmptxt = String.Format("{0:dd.MM.yyyy HH:mm:ss} {1}", currenttime, message);
+
+
+                //}
+                //else
+                //{
+                //    tmptxt = String.Format("{0:dd.MM.yyyy HH:mm:ss} {1}", currenttime, "-----Повтор---- " + message);
+
+                //}
+
+
+                clFileRW.WriteFileTXT(DateTime.Now, clWinAPI.GetCaptionOfActiveWindow());
+
                 Thread.Sleep(60000);
             }
 
---
+++ SeachActiveApp/clFileRW.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeachActiveApp
+{
+    class clFileRW
+    {
+        #region Вывод в файл
+        public static void WriteFileTXT(DateTime dt, string message)
+        {
+            try
+            {
+                if (message != "" || message != null || message != " ")
+                {
+                    string tmptxt;
+                    DateTime TimeWrite = dt;
+
+                    tmptxt = dt.ToString("dd.MM.yyyy HH:mm:ss") + " - " + message;
+
+                    //Если не удачно то записываем в локальный файл
+                    string pathProg = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "Log.txt";
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(pathProg, true))
+                    {
+
+                        file.WriteLine(tmptxt);
+                        file.Close();
+                    }
+
+                    
+                }
+
+            }
+            catch
+            { }
+        }
+
+        #endregion
+
+    }
+}

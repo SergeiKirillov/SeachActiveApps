9530d8f009105617a36a442c1f5c3abbb9fd7bdb
SergeiKirillov <sergei.kirillov.a@gmail.com>
2020-11-06 22:27:11 +0600

    1 -tray, 2-login

--- SeachActiveApp/Program.cs
+++ SeachActiveApp/Program.cs
@@ -26,72 +26,96 @@ namespace SeachActiveApp
             DateTime dtActiveApp;
             TimeSpan ts;
 
-
-           
-
-                //считывание текущих значений из реестра только в момент запуска приложения и после этого используем локальную переменную
-            strActivAppOld = clReg.ReadAppParam("NameAppOld");
-            dtAppOld = Convert.ToDateTime(clReg.ReadAppParam("dtAppOld"));
-
-
-            clWinAPI.HideConsoleApp(true); //Прячем программу
-
-
-            bool mkr = isStillRunning();
-
-            while (true)
+            
+            if (isStillRunning())
             {
-                
-                strActivApp = clWinAPI.GetCaptionOfActiveWindow();
-                dtActiveApp = DateTime.Now;
+                System.Diagnostics.Debug.WriteLine("второй раз");
 
+                frmLogin login = new frmLogin();
 
-                ts = dtActiveApp.Subtract(dtAppOld);
-
-                if (strActivAppOld != strActivApp)
+                if (login.Visible)
                 {
-
-                    if (ts.TotalMinutes > 1)
-                    {
-                        clFileRW.WriteFileTXT(dtActiveApp, strActivApp, ts);
-                        
-                        strActivAppOld = strActivApp;
-                        dtAppOld = dtActiveApp;
-
-                        //запись в реестр текущих значений
-                        clReg.WriteAppParam("ActiveApp", strActivApp);
-                        clReg.WriteAppParam("TimeActiveApp", dtActiveApp.ToString());
-
-                    }
-
-
-                }
-
-
-                if (ts.TotalHours > 1)
-                {
-                    clFileRW.WriteFileTXT(dtActiveApp, "(Работает приложение более 1 часа)" + strActivApp, ts);
-
-                    if (clReg.WriteMaxAppReg(dtAppOld, dtActiveApp, ts, strActivApp))
-                    {
-                        //сброс значений если произошла запись 
-                        
-                    }
-
-
+                    login.Focus();
                 }
                 else
                 {
-                    //если приложение не меняется то записываем в реестр значение времени его работы
-                    clReg.WriteAppParam("SpanActiveApp", ts.TotalMinutes.ToString());
+                    login.ShowDialog();
                 }
-                   
-                
 
-                
-                               
-                Thread.Sleep(60000); //спим 1 мин
+
             }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("первый раз");
+
+                //считывание текущих значений из реестра только в момент запуска приложения и после этого используем локальную переменную
+                strActivAppOld = clReg.ReadAppParam("NameAppOld");
+                dtAppOld = Convert.ToDateTime(clReg.ReadAppParam("dtAppOld"));
+
+
+                clWinAPI.HideConsoleApp(true); //Прячем программу
+
+                while (true)
+                {
+
+                    strActivApp = clWinAPI.GetCaptionOfActiveWindow();
+                    dtActiveApp = DateTime.Now;
+
+
+                    ts = dtActiveApp.Subtract(dtAppOld);
+
+                    if (strActivAppOld != strActivApp)
+                    {
+
+                        if (ts.TotalMinutes > 1)
+                        {
+                            clFileRW.WriteFileTXT(dtActiveApp, strActivApp, ts);
+
+                            strActivAppOld = strActivApp;
+                            dtAppOld = dtActiveApp;
+
+                            //запись в реестр текущих значений
+                            clReg.WriteAppParam("ActiveApp", strActivApp);
+                            clReg.WriteAppParam("TimeActiveApp", dtActiveApp.ToString());
+
+                        }
+
+
+                    }
+
+
+                    if (ts.TotalHours > 1)
+                    {
+                        clFileRW.WriteFileTXT(dtActiveApp, "(Работает приложение более 1 часа)" + strActivApp, ts);
+
+                        if (clReg.WriteMaxAppReg(dtAppOld, dtActiveApp, ts, strActivApp))
+                        {
+                            //сброс значений если произошла запись 
+
+                        }
+
+
+                    }
+                    else
+                    {
+                        //если приложение не меняется то записываем в реестр значение времени его работы
+                        clReg.WriteAppParam("SpanActiveApp", ts.TotalMinutes.ToString());
+                    }
+
+
+
+
+
+                    Thread.Sleep(60000); //спим 1 мин
+                }
+
+            }
+            
+
+
+            
+
+            
 
         }
 

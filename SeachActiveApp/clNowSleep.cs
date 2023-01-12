dfabd4a8bd9eea54af0ad5f2b003459576a0c610
serge <serge@HP-NOTE-HOME>
2023-01-12 21:06:39 +0600

    Add class clNowSleep - если запущена экранная заставка то выйти из неё

---
+++ SeachActiveApp/clNowSleep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeachActiveApp
+{
+    class clNowSleep
+    {
+/*
+ * Класс предназначен для переодического выхода из экранной заставки
+ * 
+ */
+    }
+}
+

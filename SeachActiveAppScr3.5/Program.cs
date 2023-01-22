1243bbbeeeff1960e8f5214c565da06b7e4ce9ca
sergei.kirillov.a <sergei.kirillov.a@gmail.com>
2023-01-22 15:22:18 +0600

    Add code seachActiveAppScr3.5

--- SeachActiveAppScr3.5/Program.cs
+++ SeachActiveAppScr3.5/Program.cs
@@ -11,11 +11,73 @@ namespace SeachActiveAppScr3._5
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmScreenSaver());
+
+            if (args.Length>0)
+            {
+                string firstArgument = args[0].ToLower().Trim();
+                string secondArgument = null;
+
+                if (firstArgument.Length>2)
+                {
+                    secondArgument = firstArgument.Substring(3).Trim();
+                    firstArgument = firstArgument.Substring(0, 2);
+                }
+                else if(args.Length>1)
+                {
+                    secondArgument = args[1];
+                }
+
+                if (firstArgument == "/c")
+                {
+                    //TODO configuration mode
+                }
+
+                else if (firstArgument == "/p")
+                {
+                    //TODO Preview mode
+
+                    if (secondArgument == null)
+                    {
+                        MessageBox.Show("Не был передан Handle windows окна", "ScreenSaver", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    }
+
+                    IntPtr PreviewWndHandle = new IntPtr(long.Parse(secondArgument));
+                    Application.Run(new frmScreenSaver(PreviewWndHandle));
+                }
+
+                else if (firstArgument=="/s")
+                {
+                    ShowScreenSaver();
+                    Application.Run();
+                }
+
+                else
+                {
+                    MessageBox.Show("Аргумент командной строки \"" + firstArgument + "\" не подходит под условия.", "ScreenSaver", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            else
+            {
+                ShowScreenSaver();
+                Application.Run();
+            }
+
+            
+        }
+
+        static void ShowScreenSaver()
+        {
+            foreach (Screen item in Screen.AllScreens)
+            {
+                frmScreenSaver Screen = new frmScreenSaver(item.Bounds);
+                Screen.Show();
+            }
         }
     }
 }
--- SeachActiveAppScr3.5/frmScreenSaver.Designer.cs
+++ SeachActiveAppScr3.5/frmScreenSaver.Designer.cs
@@ -29,12 +29,48 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtLabel = new System.Windows.Forms.Label();
+            this.MoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtLabel
+            // 
+            this.txtLabel.AutoSize = true;
+            this.txtLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtLabel.ForeColor = System.Drawing.Color.Red;
+            this.txtLabel.Location = new System.Drawing.Point(522, 155);
+            this.txtLabel.Name = "txtLabel";
+            this.txtLabel.Size = new System.Drawing.Size(203, 73);
+            this.txtLabel.TabIndex = 0;
+            this.txtLabel.Text = "Demo";
+            // 
+            // MoveTimer
+            // 
+            this.MoveTimer.Tick += new System.EventHandler(this.MoveTimer_Tick);
+            // 
+            // frmScreenSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmScreenSaver";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmScreenSaver_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmScreenSaver_KeyPress);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.frmScreenSaver_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmScreenSaver_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label txtLabel;
+        private System.Windows.Forms.Timer MoveTimer;
     }
 }
 

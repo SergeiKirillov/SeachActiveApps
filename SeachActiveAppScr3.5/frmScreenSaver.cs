b4a180234161120c22a2a3734d23fc62f672bf9f
sergei.kirillov.a <sergei.kirillov.a@gmail.com>
2023-01-22 14:24:41 +0600

    Add ScreenSaver NetFramework 3.5 (SeachActiveAppScr3.5)

---
+++ SeachActiveAppScr3.5/frmScreenSaver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SeachActiveAppScr3._5
+{
+    public partial class frmScreenSaver : Form
+    {
+        public frmScreenSaver()
+        {
+            InitializeComponent();
+        }
+    }
+}

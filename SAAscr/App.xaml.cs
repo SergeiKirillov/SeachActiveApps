48f42fb4f0b878792965fce0b14a237019c6f11b
serge <serge@HP-NOTE-HOME>
2023-02-05 22:09:58 +0600

    end home 05.02.2023

--- SAAscr/App.xaml.cs
+++ SAAscr/App.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Shapes;
 
 namespace SAAscr
 {
@@ -36,6 +37,26 @@ namespace SAAscr
                         break;
                     case "/p":
 
+                        MainWindow mainWindow = new MainWindow();
+
+                        if (Screen.AllScreens.Length > 1)
+                        {
+                            Screen s2 = Screen.AllScreens[1];
+                            Rectangle r2 = s2.WorkingArea;
+                            mainWindow.Top = r2.Top;
+                            mainWindow.Left = r2.Left;
+                            mainWindow.Show();
+                        }
+
+                        else
+                        {
+                            Screen s1 = Screen.AllScreens[0];
+                            Rectangle r1 = s1.WorkingArea;
+                            mainWindow.Top = r1.Top;
+                            mainWindow.Left = r1.Left;
+                            mainWindow.Show();
+                        }
+
                         break;
                     default:
                         break;
@@ -49,7 +70,9 @@ namespace SAAscr
             }
         }
 
+
         
+
 
 
     }
